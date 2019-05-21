87cd9123ddc585a1b1e6ace789dc13984db75b16
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2019-05-21 12:19:38 +0100

    Modify arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,64 +1,81 @@
 #include "TimerOne.h"
 
-#define SAMPLING_FREQUENCY  100                                     // Sampling frequency in Hz.
-#define SAMPLING_PERIOD     1000000 * 1 / SAMPLING_FREQUENCY        // Sampling period in microseconds.
-//#define SAMPLING_TIME       1                                           // Sampling time in seconds.
-//#define SAMPLING_NUMBERS    SAMPLING_TIME * SAMPLING_FREQUENCY  // Sampling number of samples.
+#define SAMPLING_FREQUENCY          100                                 // Sampling frequency in Hz.
+#define SAMPLING_PERIOD             1000000 * 1 / SAMPLING_FREQUENCY    // Sampling period in microseconds.
+#define SAMPLING_NUMBER_OF_SAMPLES  10                                   // Number of samples to be averaged.
+#define SAMPLING_DIFFERENCE_MARGIN  5                                  // A blink is detected if the difference     
 
 #define SAMPLING_INPUT    A3  // Microphone input pin.
-#define LED_OUTPUT        8   // LED output pin.
+#define BUZZER_OUTPUT     8   // Buzzer output pin.
 
 void ISR_Timer1 ();
 
-//bool sampling_done = false;
-int  sampling_index = 0;
-//int  sample = 0;
+int state_eye_open = 1;
+int sampling_threshold = 0;
+int sampling_results_index = 0;
+int sampling_results_average = 0;
+int sampling_results[SAMPLING_NUMBER_OF_SAMPLES];
+
+unsigned long time_millisecond = 0;
+unsigned long time_millisecond_last = 0;
 
 void setup()
 {
   // Print program start message.
   Serial.begin(9600);
-  Serial.println("Successful, program start!\n");
+  //Serial.println("Successful, program start!\n");
 
   // LED initialization.
-  pinMode(LED_OUTPUT, OUTPUT);
-  digitalWrite(LED_OUTPUT, LOW);
+  pinMode(BUZZER_OUTPUT, OUTPUT);
+  digitalWrite(BUZZER_OUTPUT, LOW);
 
+  // Initialize sampling results to zero.
+  int i;
+  for (i = 0; i < SAMPLING_NUMBER_OF_SAMPLES; i++) {
+    sampling_results[i] = 0; 
+  }
+  
   // Timer 1 initialization.
   Timer1.initialize();
-  delay(100);
   Timer1.attachInterrupt(ISR_Timer1, SAMPLING_PERIOD);
-
-//  int *sampling_results[SAMPLING_NUMBERS];
-//  while (!sampling_done) {
-//    //sampling_results[sampling_index] = sample;
-//  }
-//  for (int i=0; i < SAMPLING_NUMBERS; i++) {
-//      int val = sampling_results[i];
-//      Serial.println(val);
-//  }
-//
-//  // Print program finish message.
-//  Serial.println("Finished, program terminates!\n");
+  delay(1000);
 }
 
 void loop()
 {
-
+  int total = 0;
+  int i = 0;
+  for (i = 0; i < SAMPLING_NUMBER_OF_SAMPLES; i++) {
+    total += sampling_results[i];
+  }
+  sampling_results_average = total / SAMPLING_NUMBER_OF_SAMPLES;
 }
 
 void ISR_Timer1 ()
 {
-//  if (sampling_index < SAMPLING_NUMBERS) {
-//    sample = analogRead(SAMPLING_INPUT);
-//    sampling_index++;
-//  }
-//  else {
-//    Timer1.detachInterrupt();
-//    sampling_done = true;
-//    digitalWrite(LED_OUTPUT, sampling_done);
-//  }
-  Serial.println(analogRead(SAMPLING_INPUT));
-  digitalWrite(LED_OUTPUT, sampling_index % 2);
-  sampling_index++;
+  //Serial.println(analogRead(SAMPLING_INPUT));
+  
+  sampling_results[sampling_results_index++] = analogRead(SAMPLING_INPUT);
+  if (sampling_results_index == SAMPLING_NUMBER_OF_SAMPLES) sampling_results_index = 0;
+  
+  if (state_eye_open) {
+    sampling_threshold = sampling_results_average - SAMPLING_DIFFERENCE_MARGIN;
+    if (sampling_results[sampling_results_index] < sampling_threshold) {
+      //time_millisecond = millis() - time_millisecond;
+      Serial.println(time_millisecond);
+      if (millis() - time_millisecond > 1000) {
+        Serial.println(1);
+        state_eye_open = false;
+      }
+    }
+  }
+  else {
+    sampling_threshold = sampling_results_average + SAMPLING_DIFFERENCE_MARGIN;
+    if (sampling_results[sampling_results_index] > sampling_threshold) {
+      time_millisecond = millis();
+      state_eye_open = true;
+    }
+  }
+
+  //Serial.println(sampling_results[sampling_results_index]);
 }
